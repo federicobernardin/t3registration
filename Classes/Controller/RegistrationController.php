916179d174cbd8d2ed7dcf43ae3d1bd33655f1da
Federico Bernardin <federico@bernardin.it>
2014-03-20 17:50:54 +0100

    Added Report Messages

--- Classes/Controller/RegistrationController.php
+++ Classes/Controller/RegistrationController.php
@@ -1,29 +1,33 @@
 <?php
 namespace TYPO3\T3registration\Controller;
 
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2014 Federico Bernardin <federico.bernardin@immaginario.com>, BFConsulting
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+    /***************************************************************
+     *  Copyright notice
+     *
+     *  (c) 2014 Federico Bernardin <federico.bernardin@immaginario.com>, BFConsulting
+     *
+     *  All rights reserved
+     *
+     *  This script is part of the TYPO3 project. The TYPO3 project is
+     *  free software; you can redistribute it and/or modify
+     *  it under the terms of the GNU General Public License as published by
+     *  the Free Software Foundation; either version 3 of the License, or
+     *  (at your option) any later version.
+     *
+     *  The GNU General Public License can be found at
+     *  http://www.gnu.org/copyleft/gpl.html.
+     *
+     *  This script is distributed in the hope that it will be useful,
+     *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+     *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *  GNU General Public License for more details.
+     *
+     *  This copyright notice MUST APPEAR in all copies of the script!
+     ***************************************************************/
+use TYPO3\CMS\Core\FormProtection\Exception;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Error\Error;
+use TYPO3\CMS\Extbase\Error\Result;
 
 /**
  *
@@ -34,70 +38,89 @@ namespace TYPO3\T3registration\Controller;
  */
 class RegistrationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
 
-	/**
-	 * userRepository
-	 *
-	 * @var \TYPO3\T3registration\Domain\Repository\UserRepository
-	 * @inject
-	 */
-	protected $userRepository;
+    /**
+     * userRepository
+     *
+     * @var \TYPO3\T3registration\Domain\Repository\UserRepository
+     * @inject
+     */
+    protected $userRepository;
 
-	/**
-	 * action new
-	 *
-	 * @param \TYPO3\T3registration\Domain\Model\User $newUser
-	 * @dontvalidate $newUser
-	 * @return void
-	 */
-	public function newAction(\TYPO3\T3registration\Domain\Model\User $newUser = NULL) {
-		$this->view->assign('newUser', $newUser);
-	}
+    /**
+     * report
+     *
+     * @var \TYPO3\T3registration\Report\Report
+     */
+    protected $report;
 
-	/**
-	 * action create
-	 *
-	 * @param \TYPO3\T3registration\Domain\Model\User $newUser
-	 * @return void
-	 */
-	public function createAction(\TYPO3\T3registration\Domain\Model\User $newUser) {
-		$this->userRepository->add($newUser);
-		$this->flashMessageContainer->add('Your new User was created.');
-		$this->redirect('list');
-	}
+    /**
+     * action new
+     *
+     * @param \TYPO3\T3registration\Domain\Model\User $newUser
+     * @dontvalidate $newUser
+     * @return void
+     */
+    public function newAction(\TYPO3\T3registration\Domain\Model\User $newUser = NULL) {
+        $this->checkConfiguration();
+        $this->view->assign('newUser', $newUser);
+    }
 
-	/**
-	 * action edit
-	 *
-	 * @param \TYPO3\T3registration\Domain\Model\User $user
-	 * @return void
-	 */
-	public function editAction(\TYPO3\T3registration\Domain\Model\User $user) {
-		$this->view->assign('user', $user);
-	}
+    private function checkConfiguration(){
+        $this->report = new \TYPO3\T3registration\Report\Report();
+        $this->report->setView($this->view);
+        $this->report->checkConfiguration($this->settings);
+        $this->response->addAdditionalHeaderData('<link rel="stylesheet" type="text/css" href="' . ExtensionManagementUtility::siteRelPath('t3skin') . 'stylesheets/structure/element_message.css" />');
+        $this->response->addAdditionalHeaderData('<link rel="stylesheet" type="text/css" href="' . ExtensionManagementUtility::siteRelPath('t3skin') . 'stylesheets/visual/element_message.css" />');
+        $this->response->addAdditionalHeaderData('<link rel="stylesheet" type="text/css" href="' . ExtensionManagementUtility::siteRelPath('t3registration') . 'Resources/Public/css/message.css" />');
+    }
 
-	/**
-	 * action update
-	 *
-	 * @param \TYPO3\T3registration\Domain\Model\User $user
-	 * @return void
-	 */
-	public function updateAction(\TYPO3\T3registration\Domain\Model\User $user) {
-		$this->userRepository->update($user);
-		$this->flashMessageContainer->add('Your User was updated.');
-		$this->redirect('list');
-	}
+    /**
+     * action create
+     *
+     * @param \TYPO3\T3registration\Domain\Model\User $newUser
+     * @return void
+     */
+    public function createAction(\TYPO3\T3registration\Domain\Model\User $newUser) {
+        $this->userRepository->add($newUser);
+        $this->flashMessageContainer->add('Your new User was created.');
+        $this->redirect('list');
+    }
 
-	/**
-	 * action delete
-	 *
-	 * @param \TYPO3\T3registration\Domain\Model\User $user
-	 * @return void
-	 */
-	public function deleteAction(\TYPO3\T3registration\Domain\Model\User $user) {
-		$this->userRepository->remove($user);
-		$this->flashMessageContainer->add('Your User was removed.');
-		$this->redirect('list');
-	}
+    /**
+     * action edit
+     *
+     * @param \TYPO3\T3registration\Domain\Model\User $user
+     * @return void
+     */
+    public function editAction(\TYPO3\T3registration\Domain\Model\User $user) {
+        $this->view->assign('user', $user);
+    }
+
+    /**
+     * action update
+     *
+     * @param \TYPO3\T3registration\Domain\Model\User $user
+     * @return void
+     */
+    public function updateAction(\TYPO3\T3registration\Domain\Model\User $user) {
+        $this->userRepository->update($user);
+        $this->flashMessageContainer->add('Your User was updated.');
+        $this->redirect('list');
+    }
+
+    /**
+     * action delete
+     *
+     * @param \TYPO3\T3registration\Domain\Model\User $user
+     * @return void
+     */
+    public function deleteAction(\TYPO3\T3registration\Domain\Model\User $user) {
+        $this->userRepository->remove($user);
+        $this->flashMessageContainer->add('Your User was removed.');
+        $this->redirect('list');
+    }
+
 
 }
+
 ?>
---
+++ Classes/Report/Report.php
@@ -0,0 +1,126 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) Federico Bernardin 2014
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+
+
+namespace TYPO3\T3registration\Report;
+
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Error\Error;
+use TYPO3\CMS\Extbase\Error\Result;
+
+/**
+ * Class Report
+ *
+ * @author Federico Bernardin <federico@bernardin.it>
+ * @package TYPO3\T3registration\Report
+ *
+ */
+class Report {
+
+    const DUPLICATE_FIELD = 'Duplicate Field';
+    const DUPLICATE_USERNAME_FIELD = 'Duplicate Username Field';
+
+    /**
+     * @var \TYPO3\CMS\Extbase\Error\Result
+     */
+    private $result = null;
+
+    /**
+     * The current view, as resolved by resolveView()
+     *
+     * @var \TYPO3\CMS\Extbase\Mvc\View\ViewInterface
+     * @api
+     */
+    protected $view = NULL;
+
+    /**
+     * Typoscript Settings
+     * @var array
+     */
+    private $settings = array();
+
+    /**
+     * Controller View
+     * @param \TYPO3\CMS\Extbase\Mvc\View\ViewInterface $view
+     */
+    public function setView($view) {
+        $this->view = $view;
+    }
+
+
+    /**
+     * Main report function
+     * @param array $settings
+     */
+    public function checkConfiguration(array $settings) {
+        $this->result = new Result();
+        $this->settings = $settings;
+        $this->testDuplicate();
+        $this->testForUniqueUsernameField();
+        $this->view->assign('messages', $this->result);
+        if(!GeneralUtility::cmpIP(GeneralUtility::getIndpEnv('REMOTE_ADDR'), $GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'])){
+            throw new \Exception('T3Registration misconfiguration');
+        }
+    }
+
+    /**
+     * Checks for field duplication
+     */
+    private function testDuplicate() {
+        $unique = array();
+        $duplicated = array();
+        foreach ($this->settings['fields'] as $field) {
+            if (in_array($field['databaseField']['name'], $unique)) {
+                $unique[] = $field['databaseField']['name'];
+            } else {
+                $duplicated[$field['databaseField']['name']] = $field['databaseField']['name'];
+            }
+        }
+        if (count($duplicated)) {
+            foreach ($duplicated as $field) {
+                $this->result->addError(new Error(sprintf('The %s field are duplicated in flexform, you must fix it.', $field), self::DUPLICATE_FIELD, array(), $field));
+            }
+        }
+    }
+
+    /**
+     * Check for fields with setAsUsername check enable, raise error if more than one
+     */
+    private function testForUniqueUsernameField() {
+        $setAsUsername = array();
+        foreach ($this->settings['fields'] as $field) {
+            if ($field['databaseField']['useAsUsername']) {
+                $setAsUsername[] = $field['databaseField']['name'];
+            }
+        }
+        if (count($setAsUsername) > 1) {
+            $this->result->addError(new Error(sprintf('This fields %s are set as username but only one can be username field, you must fix it.', implode(',', $setAsUsername)), self::DUPLICATE_USERNAME_FIELD, array(), implode(',', $setAsUsername)));
+        }
+    }
+}
