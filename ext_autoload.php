04d8598b1cb7d4f89e159aea6b1554feb2ac58b0
Federico Bernardin <federico@bernardin.it>
2014-06-03 22:41:20 +0200

    Removed unused code

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,6 +1,6 @@
 <?php
 //require_once(PATH_site . 'typo3temp/Cache/Code/cache_phpcode/user.php');
-$pippo = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('t3registration') . 'Classes/Cache/CacheBuilder.php';
+//$pippo = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('t3registration') . 'Classes/Cache/CacheBuilder.php';
 require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('t3registration') . 'Classes/Cache/CacheBuilder.php');
 $cacheBuilder = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\T3registration\Cache\CacheBuilder');
 return $cacheBuilder->build();
